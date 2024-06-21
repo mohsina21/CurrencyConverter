6a7c87b95703853adf5bec2cf0203372b45c41c9
Mohsina Parveen <159033605+mohsina21@users.noreply.github.com>
2024-06-21 19:29:44 +0530

    creaated component CurrencyBtn

--- src/components/CurrencyBtn.tsx
+++ src/components/CurrencyBtn.tsx
@@ -1,12 +1,35 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
-export default function CurrencyBtn() {
-  return (
-    <View>
-      <Text>CurrencyBtn</Text>
+import type { PropsWithChildren } from 'react';
+type CurrencyBtnProps = PropsWithChildren<{
+  name : 'string';
+  flag: 'string'
+}>
+const CurrencyBtn = (props: CurrencyBtnProps): JSX.Element =>{
+  return(
+    <View style = {styles.BtnContainer}>
+      <Text style = {styles.flag}>{props.flag} </Text>
+      <Text style = {styles.name}>{props.name} </Text>
     </View>
   )
+
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+BtnContainer: {
+alignItems: 'center'
+},
+flag:{
+  fontSize: 24,
+  color: "#FFFFFF",
+  marginBottom: 5
+},
+name: {
+  fontSize: 10,
+  color: "#2d3436"
+
+}
+
+})
+export default CurrencyBtn
+
