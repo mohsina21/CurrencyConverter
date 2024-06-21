522258ade6908c29adeea28cd54e5b8a541f7216
Mohsina Parveen <159033605+mohsina21@users.noreply.github.com>
2024-06-21 10:39:48 +0530

    comit

---
+++ src/components/CurrencyBtn.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function CurrencyBtn() {
+  return (
+    <View>
+      <Text>CurrencyBtn</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
