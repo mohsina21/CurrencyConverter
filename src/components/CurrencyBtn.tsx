1545b7eabee2e49ab02addb1cfa57e1922b4c7cb
Mohsina Parveen <159033605+mohsina21@users.noreply.github.com>
2024-06-23 19:04:34 +0530

    updated currencybtn

--- src/components/CurrencyBtn.tsx
+++ src/components/CurrencyBtn.tsx
@@ -1,35 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 import type { PropsWithChildren } from 'react';
-type CurrencyBtnProps = PropsWithChildren<{
-  name : 'string';
-  flag: 'string'
-}>
-const CurrencyBtn = (props: CurrencyBtnProps): JSX.Element =>{
-  return(
-    <View style = {styles.BtnContainer}>
-      <Text style = {styles.flag}>{props.flag} </Text>
-      <Text style = {styles.name}>{props.name} </Text>
-    </View>
-  )
 
-}
+type CurrencyBtnProps = PropsWithChildren<{
+  name: string;
+  flag: string;
+}>;
+
+const CurrencyBtn = (props: CurrencyBtnProps): JSX.Element => {
+  return (
+    <View style={styles.BtnContainer}>
+      <Text style={styles.flag}>{props.flag}</Text>
+      <Text style={styles.name}>{props.name}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-BtnContainer: {
-alignItems: 'center'
-},
-flag:{
-  fontSize: 24,
-  color: "#FFFFFF",
-  marginBottom: 5
-},
-name: {
-  fontSize: 10,
-  color: "#2d3436"
+  BtnContainer: {
+    alignItems: 'center',
+  },
+  flag: {
+    fontSize: 24,
+    color: "#FFFFFF",
+    marginBottom: 5,
+  },
+  name: {
+    fontSize: 10,
+    color: "#2d3436",
+  },
+});
 
-}
+export default CurrencyBtn;
 
-})
-export default CurrencyBtn
 
